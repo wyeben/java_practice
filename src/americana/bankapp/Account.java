b9d22b9f268a90ce1a7712e3f52ac151862c8a47
Wyeben <yila.ben@yahoo.com>
2023-11-25 21:39:22 +0100

    added code

--- src/americana/bankapp/Account.java
+++ src/americana/bankapp/Account.java
@@ -29,6 +29,7 @@ public class Account {
     public void withdraw(double amount){
         if(balance > amount){
             balance -= amount;
+            System.out.println("");
         }
     }
 }
