ba8cf03d25d7e4f54a1c09940b3e4ceff15a510c
Wyeben <yila.ben@yahoo.com>
2023-12-03 09:44:13 +0100

    nokia 3310

--- src/americana/nokia3310/Nokia3310.java
+++ src/americana/nokia3310/Nokia3310.java
@@ -5,7 +5,27 @@ import java.util.Scanner;
 public class Nokia3310 {
     public static void main(String[] args) {
         System.out.println("Nokia");
+        System.out.print("""
+                Select
+                1. Phone Book
+                2. Messages
+                3. Chat
+                4. Call Register
+                5. Tones
+                6. Settings
+                7. Call Divert
+                8. Games
+                9. Calculator
+                10. Reminders
+                11. Clock
+                12. Profiles
+                13. Sim Services""");
+
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
+
+//        switch (userInput){
+//            case
+//        }
     }
 }
