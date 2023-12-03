0a401e857106401e623bf64da655f0d5eab91101
Wyeben <yila.ben@yahoo.com>
2023-12-03 09:27:05 +0100

    nokia 3310

---
+++ src/americana/nokia3310/Nokia3310.java
@@ -0,0 +1,11 @@
+package americana.nokia3310;
+
+import java.util.Scanner;
+
+public class Nokia3310 {
+    public static void main(String[] args) {
+        System.out.println("Nokia");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+    }
+}
