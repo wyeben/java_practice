0d3bdf89239533b6a5181373e5ef72125c8c8550
Wyeben <yila.ben@yahoo.com>
2023-12-04 07:33:18 +0100

    nokia 3310

--- src/americana/nokia3310/Nokia3310.java
+++ src/americana/nokia3310/Nokia3310.java
@@ -55,10 +55,14 @@ public class Nokia3310 {
         switch (userInput) {
             case 1:
                 System.out.println("Sim inserted successfully");
-
+                break;
             case 2:
                 System.out.println("Displaying menu...");
                 nokia3310.menu();
+                break;
+            default:
+                System.out.println("Invalid input");
+                break;
         }
     }
 
